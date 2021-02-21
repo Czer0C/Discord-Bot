2cc7cc1a2ecf9807e5e3091afd3ccd3eeb15d14a
Nguyễn Hải Triều <36495153+Czer0C@users.noreply.github.com>
2021-02-21 16:43:36 +0700

    refactor

--- commands/main/wiki.js
+++ commands/main/wiki.js
@@ -45,25 +45,7 @@ module.exports = {
             
         } catch (error) { // Fallback to searching for articles related to keyword
             if (error.response && error.response.status === 404) { 
-                const searchResults = await axios(encodeURI(query)).then(site => site.data);
-
-                const $ = cheerio.load(searchResults);
-
-                const list = $('.unified-search__result__title');
-
-                let board = [];
-
-                for (let i = 0; i < list.length; i++) {
-                    const {
-                        href, 
-                        "data-title": title
-                    } = list[i].attribs;
-
-                    board.push({href, title});
-                }
-
-                let content = board.slice(0, 10).reduce(
-                    (acc, row) => acc + `[${row.title}](${row.href})\n`, '');
+                const content = await searchForRelatedArticles(query);
                 
                 const response = embed({
                     color: "#541f1f",
@@ -109,4 +91,26 @@ parseDirectPage = async (url, keyword) => {
         pictureLink,
         wikiEmbedContent
     }
+}
+
+searchForRelatedArticles = async (query) => {
+    const searchResults = await axios(encodeURI(query)).then(site => site.data);
+
+    const $ = cheerio.load(searchResults);
+
+    const list = $('.unified-search__result__title');
+
+    let board = [];
+
+    for (let i = 0; i < list.length; i++) {
+        const {
+            href, 
+            "data-title": title
+        } = list[i].attribs;
+
+        board.push({href, title});
+    }
+
+    return board.slice(0, 10).reduce(
+        (acc, row) => acc + `[${row.title}](${row.href})\n`, '');
 }
